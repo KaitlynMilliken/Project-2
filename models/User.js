4e87b29a494ca527024f346fd541057fc003ded6
elijahb22 <elibusick11@gmail.com>
2022-02-10 20:09:28 -0500

    added bycrypt to hash password

--- models/User.js
+++ models/User.js
@@ -36,11 +36,24 @@ User.init(
         }     
     },
     {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'user'
+
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+        
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+        
     }
 );
 
