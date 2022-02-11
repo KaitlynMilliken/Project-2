e407b662c31d94291a938a79815ff061d39ef77a
elijahb22 <elibusick11@gmail.com>
2022-02-10 20:57:48 -0500

    added has and finished db set ip for admin. also exported to index file

--- models/admin.js
+++ models/admin.js
@@ -36,4 +36,24 @@ admin.init(
             }
         }     
     },
+    {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+        
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'admin'    
+    }
 );
+
+module.exports = admin;
