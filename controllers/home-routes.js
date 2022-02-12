1aac229d46b1b501e3c9f99f71326683a947389e
elijahb22 <elibusick11@gmail.com>
2022-02-12 15:54:37 -0500

    connected models to home routes page and created get route for login

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
-
+const { User, post, comment, admin } = require('../models');
 router.get('/', (req, res) => {
   res.render('homepage');
 });
-
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
 module.exports = router;
