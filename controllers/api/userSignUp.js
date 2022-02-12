ecb0e99b2d9275fbf9db2dab8924ddee483dfde7
Kaitlyn Milliken <kaitlynmilliken@gmail.com>
2022-02-12 15:16:54 -0500

    added sign up and login in feature

--- controllers/api/userSignUp.js
+++ controllers/api/userSignUp.js
@@ -1,11 +1,52 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-    res.render('userSignUp');
+router.post('/', (req, res) => {
+    // User.create({
+    //     username: req.body.username,
+    //     email: req.body.email,
+    //     password: req.body.password
+    // })
+    //     .then(dbUserData => res.json(dbUserData))
+    //     .catch(err => {
+    //         console.log(err);
+    //         res.status(500).json(err);
+    //     });
+    console.log(req.body);
 });
 
-router.get('/signup', (req,res) => {
-    console.log("woo!");
+router.post('/login', (req, res) => {
+    // User.findOne({
+    //     where: {
+    //         email: req.body.email
+    //     }
+    // }).then(dbUserData => {
+    //     if (!dbUserData) {
+    //         res.status(400).json({ message: 'No user with that email address!' });
+    //         return;
+    //     }
+
+    //     const validPassword = dbUserData.checkPassword(req.body.password);
+
+    //     if (!validPassword) {
+    //         res.status(400).json({ message: 'Incorrect password!' });
+    //         return;
+    //     }
+
+    //     req.session.save(() => {
+    //         // declare session variables
+    //         req.session.user_id = dbUserData.id;
+    //         req.session.username = dbUserData.username;
+    //         req.session.loggedIn = true;
+
+    //         res.json({ user: dbUserData, message: 'You are now logged in!' });
+    //     });
+    // });
+
+    console.log(req.body);
+});
+
+router.get('/', (req, res) => {
+    res.render('userSignUp');
 });
 
 module.exports = router;
