3649af75a280760baea862f2663a5b076d078e38
Kaitlyn Milliken <kaitlynmilliken@gmail.com>
2022-02-12 16:48:30 -0500

    added new post abilities

--- controllers/api/posts.js
+++ controllers/api/posts.js
@@ -1,7 +1,17 @@
 const router = require('express').Router();
+const { Post } = require('../../models');
 
 router.post('/test', (req, res) => {
-    console.log("ya submitted!");
+    Post.create({
+        title: req.body.postTitle,
+        postBody: req.body.postBody
+    })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    console.log(req.body);
 });
 
 module.exports = router;
