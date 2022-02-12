1cda3e8bb624be49f58162313e8baef599263924
elijahb22 <elibusick11@gmail.com>
2022-02-12 16:14:43 -0500

    added user route and connected models

--- controllers/api/userSignUp.js
+++ controllers/api/userSignUp.js
@@ -1,46 +1,57 @@
 const router = require('express').Router();
+const { User, comment, post } = require('../../models')
 
+router.get('/', (req, res) => {
+    User.findAll({
+      attributes: { exclude: ['password'] }
+    })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
 router.post('/', (req, res) => {
-    // User.create({
-    //     username: req.body.username,
-    //     email: req.body.email,
-    //     password: req.body.password
-    // })
-    //     .then(dbUserData => res.json(dbUserData))
-    //     .catch(err => {
-    //         console.log(err);
-    //         res.status(500).json(err);
-    //     });
+     User.create({
+         username: req.body.username,
+         email: req.body.email,
+         password: req.body.password
+     })
+         .then(dbUserData => res.json(dbUserData))
+         .catch(err => {
+             console.log(err);
+             res.status(500).json(err);
+     });
     console.log(req.body);
 });
 
 router.post('/login', (req, res) => {
-    // User.findOne({
-    //     where: {
-    //         email: req.body.email
-    //     }
-    // }).then(dbUserData => {
-    //     if (!dbUserData) {
-    //         res.status(400).json({ message: 'No user with that email address!' });
-    //         return;
-    //     }
+    User.findOne({
+         where: {
+             email: req.body.email
+         }
+    }).then(dbUserData => {
+         if (!dbUserData) {
+             res.status(400).json({ message: 'No user with that email address!' });
+             return;
+         }
 
-    //     const validPassword = dbUserData.checkPassword(req.body.password);
+         const validPassword = dbUserData.checkPassword(req.body.password);
 
-    //     if (!validPassword) {
-    //         res.status(400).json({ message: 'Incorrect password!' });
-    //         return;
-    //     }
+        if (!validPassword) {
+             res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
 
-    //     req.session.save(() => {
-    //         // declare session variables
-    //         req.session.user_id = dbUserData.id;
-    //         req.session.username = dbUserData.username;
-    //         req.session.loggedIn = true;
+        req.session.save(() => {
+             // declare session variables
+             req.session.user_id = dbUserData.id;
+             req.session.username = dbUserData.username;
+             req.session.loggedIn = true;
 
-    //         res.json({ user: dbUserData, message: 'You are now logged in!' });
-    //     });
-    // });
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+    });
 
     console.log(req.body);
 });
