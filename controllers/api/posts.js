06efc7e681a83fb92ee208600a780b3bc021368b
Kaitlyn Milliken <kaitlynmilliken@gmail.com>
2022-02-14 20:59:49 -0500

    added user_id to posts

--- controllers/api/posts.js
+++ controllers/api/posts.js
@@ -4,14 +4,14 @@ const { Post } = require('../../models');
 router.post('/newPost', (req, res) => {
     Post.create({
         title: req.body.title,
-        description: req.body.description
+        description: req.body.description,
+        user_id: req.session.user_id
     })
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
         });
-    console.log(req.body);
 });
 
 module.exports = router;
--- models/post.js
+++ models/post.js
@@ -1,7 +1,7 @@
-const { Model, DataTypes } = require('Sequelize');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Post extends Model { };
+class Post extends Model {};
 
 Post.init(
     {
@@ -19,12 +19,12 @@ Post.init(
             type: DataTypes.STRING,
             allowNull: false,
         },
-        userId: {
+        user_id: {
             type: DataTypes.INTEGER,
             references: {
                 model: 'user',
                 key: 'id',
-            },
+            }
         },
     },
     {
