9d83d0f11efb79ee9d64a5923f9a403c491bc969
elijahb22 <elibusick11@gmail.com>
2022-02-15 20:43:40 -0500

    add admin signup

---
+++ controllers/api/adminSignUp.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const { Admin, comment, post } = require('../../models')
+
+router.post('/', (req, res) => {
+    Admin.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+    .then(dbAdminData => {
+     req.session.save(() => {
+       req.session.admin_id = dbAdminData.id;
+       req.session.username = dbUserData.username;
+       req.session.loggedIn = true;
+   
+       res.json(dbAdminData);
+     });
+   })
+     .catch(err => {
+         console.log(err);
+         res.status(500).json(err);
+    });
+   console.log(req.body);
+});
+
+module.exports = router; 
